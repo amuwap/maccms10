2d337d2437cf845954753b886040f80bf72f8ccd
amuwap <amuwap@users.noreply.github.com>
2026-04-09 12:30:51 +0000

    feat: 苹果CMS影视站二开与模板设计
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- application/database.php
+++ application/database.php
@@ -1,15 +1,22 @@
 <?php
+/**
+ * 数据库配置文件
+ * 作者：阿木
+ * 网址：Amu5.Com
+ * QQ：46552292
+ * 请根据您的实际数据库信息修改以下配置
+ */
 return [
     'type'            => 'mysql',
-    'hostname'        => '127.0.0.1',
-    'database'        => 'maccms10',
-    'username'        => 'root',
-    'password'        => '',
-    'hostport'        => '3306',
+    'hostname'        => '127.0.0.1',  // 数据库服务器地址
+    'database'        => 'maccms10',     // 数据库名称
+    'username'        => 'root',          // 数据库用户名
+    'password'        => '',              // 数据库密码（请填入您的实际密码）
+    'hostport'        => '3306',          // 数据库端口
     'dsn'             => '',
     'params'          => [],
-    'charset'         => 'utf8mb4',
-    'prefix'          => 'mac_',
+    'charset'         => 'utf8mb4',       // 字符集（支持emoji）
+    'prefix'          => 'mac_',          // 表前缀
     'debug'           => false,
     'deploy'          => 0,
     'rw_separate'     => false,
